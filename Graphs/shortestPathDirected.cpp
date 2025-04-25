1281bb80f63bf125778d5d132f7a8899a7caf844
MD Rizwan <124679577+RiZwAn755@users.noreply.github.com>
2025-04-25 18:17:51 +0530

    Update shortestPathDirected.cpp

--- Graphs/shortestPathDirected.cpp
+++ Graphs/shortestPathDirected.cpp
@@ -12,7 +12,8 @@ class Solution {
     vector<int>dist;
     
     
-    void dfs(int i , stack<int>& st) // for topo sort
+    void dfs(int i , stack<int>& st) // for topo sort.
+    // we used toposort to ensure that the node on which we sre currently have its parents correct distance already
     {
         vis[i] = 1;
         for(auto it : g[i])
