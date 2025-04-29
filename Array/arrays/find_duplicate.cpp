9c8483ecb7b485a87398540c7290c1a6c69b3e64
MD Rizwan <124679577+RiZwAn755@users.noreply.github.com>
2025-04-29 16:45:00 +0530

    Update find_duplicate.cpp

--- Array/arrays/find_duplicate.cpp
+++ Array/arrays/find_duplicate.cpp
@@ -8,61 +8,63 @@
 // slow = nums[slow] and fast = nums[nums[fast]] , while slow !=  fast 
 
 
-// class Solution {
-//     public:
-//         int findDuplicate(vector<int>& nums) 
-//         {
-//             int slow = nums[0];
-//             int fast = nums[0];
+class Solution {
+    public:
+        int findDuplicate(vector<int>& nums) 
+        {
+            int slow = nums[0];
+            int fast = nums[0];
     
-//            do
-//             {
-//                 slow = nums[slow];
-//                 fast = nums[nums[fast]];
-//             } while(slow != fast);
+           do
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            } while(slow != fast);
     
-//             fast = nums[0];
-//             while(slow != fast)
-//             {
-//                 slow = nums[slow];
-//                 fast = nums[fast];
-//             }
+            fast = nums[0];
+            while(slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
     
-//             return slow;
-//         }
-//     };
+            return slow;
+        }
+    };
 
 // Aprroach 2 : 
 
 
-// vector<int> Solution::repeatedNumber(const vector<int> &arr) {
-//     int n = arr.size();
+vector<int> Solution::repeatedNumber(const vector<int> &arr) {
+    int n = arr.size();
  
-//  int repeating = -1;
+ int repeating = -1;
 
-//  for (int i = 0; i < n; i++) {
-//      int val = abs(arr[i]);
+ for (int i = 0; i < n; i++) 
+ {
+     int val = abs(arr[i]);
      
-//      if (arr[val - 1] > 0) {
-//          arr[val - 1] = -arr[val - 1];
-//      }
-//      else {
-         
-//          // ELement is repeating.
-//          repeating = val;
-//      }
-//  }
+     if (arr[val - 1] > 0) 
+     {
+         arr[val - 1] = -arr[val - 1];
+     }
+     else 
+     {         
+         // ELement is repeating.
+         repeating = val;
+     }
+ }
  
-//  int missing = -1;
+ int missing = -1;
  
-//  // Value at missing value index
-//  // will be positive.
-//  for (int i=0; i<n; i++) {
-//      if (arr[i] > 0) {
-//          missing = i+1;
-//          break;
-//      }
-//  }
+ // Value at missing value index
+ // will be positive.
+ for (int i=0; i<n; i++) {
+     if (arr[i] > 0) {
+         missing = i+1;
+         break;
+     }
+ }
  
-//  return {repeating, missing};
-// }
+ return {repeating, missing};
+}
