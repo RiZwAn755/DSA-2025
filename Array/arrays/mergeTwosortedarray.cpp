c77949921ac26f6534f9d8d7fec3f84c45896b80
MD Rizwan <124679577+RiZwAn755@users.noreply.github.com>
2025-04-29 16:53:27 +0530

    Update mergeTwosortedarray.cpp

--- Array/arrays/mergeTwosortedarray.cpp
+++ Array/arrays/mergeTwosortedarray.cpp
@@ -1,28 +1,28 @@
 
 // problem : to merge two sorted array without extra space
-// class Solution {
-//     public:
-//         void merge(vector<int>& nums1, int n1, vector<int>& nums2, int n2) 
-//         {
-//             int i = n1-1;
-//             int j = n2-1;
-//             int k = n1+n2-1;
+class Solution {
+    public:
+        void merge(vector<int>& nums1, int n1, vector<int>& nums2, int n2) 
+        {
+            int i = n1-1; // last index of first array
+            int j = n2-1; // last index of second array
+            int k = n1+n2-1; // last index of merged array
     
     
-//             // nums2 ko khali kar rhe h
-    
-//             while(j >= 0)
-//             {
-//                 if(i >=0 && nums1[i] > nums2[j])
-//                 {
-//                     nums1[k--] = nums1[i--];
-//                 }
-//                 else
-//                 {
-//                     nums1[k--] = nums2[j--];
-//                 }
-//             }
+            // nums2 ko khali kar rhe h
+            // nums1 me hi nums2 ko merge kar rhe hai
+            while(j >= 0)
+            {
+                if(i >=0 && nums1[i] > nums2[j])
+                {
+                    nums1[k--] = nums1[i--];
+                }
+                else
+                {
+                    nums1[k--] = nums2[j--];
+                }
+            }
     
            
-//         }
-//     };
+        }
+    };
