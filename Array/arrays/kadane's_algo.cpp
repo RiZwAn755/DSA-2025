15945dae69584190a7fdce2f7f101b6a4cf9dca5
MD Rizwan <124679577+RiZwAn755@users.noreply.github.com>
2025-04-29 16:46:45 +0530

    Update kadane's_algo.cpp

--- Array/arrays/kadane's_algo.cpp
+++ Array/arrays/kadane's_algo.cpp
@@ -7,31 +7,29 @@
 // approach : lets make two vars sum = 0 and maxi = -INF, and iterate the nums and add each element to sum if sum > maxi , set maxi to sum ,
 // if at any point sum becomes less than zero we set sum = 0 , (i.e hame kisi index k element ko add karne pr sum less ho rha h to ham wha se nya sum start krenge )
 
-// class Solution {
-//     public:
-//         int maxSubArray(vector<int>& nums) 
-//         {
-//             int n = nums.size();
-//             long long maxi = LONG_MIN; 
-//             long long sum = 0;
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) 
+        {
+            int n = nums.size();
+            long long maxi = LONG_MIN; 
+            long long sum = 0;
     
-//         for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
     
-//             sum += nums[i];
+            sum += nums[i];
     
-//             if (sum > maxi) {
-//                 maxi = sum;
-//             }
+            if (sum > maxi) {
+                maxi = sum;
+            }
     
-//             if (sum < 0) 
-//             {                         // arr: 2 , -3 , 4 // sum_before : 2 , -1 , 3
-//                                                   //sum after: 2 , 0 , 4
-//                 sum = 0;
-//             }
-//         }
-    
-    
-//         return maxi;
-//         }
-//     };
+            if (sum < 0) 
+            {                         // arr: 2 , -3 , 4 // sum_before : 2 , -1 , 3
+                                                  //sum after: 2 , 0 , 4
+                sum = 0;
+            }
+        }
+        return maxi;
+        }
+    };
 
