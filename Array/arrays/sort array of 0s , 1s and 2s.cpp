b78ea768c0f7e4f3816b7b310b073bfbd83f8fbb
MD Rizwan <124679577+RiZwAn755@users.noreply.github.com>
2025-05-22 12:25:15 +0530

    Update sort array of 0s , 1s and 2s.cpp

--- Array/arrays/sort array of 0s , 1s and 2s.cpp
+++ Array/arrays/sort array of 0s , 1s and 2s.cpp
@@ -2,9 +2,9 @@
 // problem : given an array which only contains 0,1 and 2 we need to sort them .
 // approach1:  directly sort => nlogn
 //approach2 : take three pointers i = 0 , j = 0 and k = n-1 , i for zeros , j  for 1s and k for 2s.
-// agar j pr zero ata h to ham j ko ek step age badha denge
+// agar j pr one ata h to ham j ko ek step age badha denge
 // agr j pr zero aya to use i se swap kar denge, aur i, j dono ko badhayenge.
-// agr j pr 2 aa gya to k se swap kr denge , aur jo ko badhayneg aur k ko kam krenge
+// agr j pr 2 aa gya to k se swap kr denge , aur j ko badhayneg aur k ko kam krenge
 
 
 // code:
@@ -27,9 +27,7 @@
     
 //                 else if(nums[j] == 1)
 //                 {
-                  
 //                     j++;
-                   
 //                 }
 //               else 
 //               {
