73200d4963a64df3815a7b5c510d308b3060cdda
MD Rizwan <124679577+RiZwAn755@users.noreply.github.com>
2025-06-19 14:10:00 +0530

    Update flatten_Binary_Tree.cpp

--- trees/flatten_Binary_Tree.cpp
+++ trees/flatten_Binary_Tree.cpp
@@ -1,28 +1,26 @@
- void preOrder(TreeNode* root , TreeNode*& ans)
+// flattening tree in linkedList like structure, nodes from preOrder traversal 
+
+
+void preOrder(TreeNode* root , TreeNode*& ans)
       {
         if(!root) return ;
-        // store them so that we wont loose their access.
+        
         TreeNode* left = root -> left;
         TreeNode* right = root -> right;
 
-       ans -> left = nullptr ;
-        ans -> right = root;
-        ans = ans -> right;
+        ans -> right = root ;
+        ans -> left = nullptr;
+        ans = ans -> right ;
 
-      if(root -> left)  preOrder( left , ans );
-       if(root -> right) preOrder(right , ans);
+        preOrder(left , ans);
+        preOrder(right , ans);
 
       }
     void flatten(TreeNode* root) 
     {
-        if(!root) return ;
-
         TreeNode* ans = new TreeNode(-1);
         TreeNode* res = ans;
 
-        preOrder(root , res);
-        root -> left = nullptr;
-        root = ans -> right -> right ; 
-        
-
+        preOrder(root , ans);
+        root = res -> right;
     }
